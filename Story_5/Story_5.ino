43f01b1f56b5f483947253d12459e9549ceca8e1
ledionselimi <22ledionselimi@treviglas.cornwall.sch.uk>
2026-02-18 16:41:18 +0000

    Story_5 added

--- Story_5/Story_5.ino
+++ Story_5/Story_5.ino
@@ -7,7 +7,6 @@ int rev_right = 8;
 
 void loop()
 {
-  Story_4();
   Story_5();
 }
 
@@ -21,26 +20,6 @@ pinMode(stop_left, OUTPUT);
 pinMode(stop_right, OUTPUT);
 pinMode(fwd_right, OUTPUT);
 pinMode(rev_right, OUTPUT);
-}
-
-// Functions
-
-void Story_4(){
-digitalWrite(fwd_left, HIGH);
-digitalWrite(fwd_right, HIGH);
-delay(5000); // Wait for 5000 millisecond(s)
-digitalWrite(fwd_left, LOW);
-digitalWrite(fwd_right,LOW);
-digitalWrite(stop_left, HIGH);
-digitalWrite(stop_right, HIGH);
-delay(1000); // Wait for 1000 millisecond(s)
-digitalWrite(stop_left, LOW);
-digitalWrite(stop_right, LOW);
-digitalWrite(rev_left, HIGH);
-digitalWrite(rev_right, HIGH);
-delay(2000); // Wait for 2000 millisecond(s)
-digitalWrite(rev_left, LOW);
-digitalWrite(rev_right, LOW);           
 }
 
 // Functions
